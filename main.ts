7e4f639fd09af706f2d7e59a56ca27997a597a76
SaganE7 <58539537+SaganE7@users.noreply.github.com>
2023-01-28 23:15:23 -0800

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,29 @@
 namespace SpriteKind {
     export const Decoration = SpriteKind.create()
+    export const Background = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    gamestarted = 0
+    candostuff = 0
+    canyesno = 0
     sprites.destroyAllSpritesOfKind(SpriteKind.Player)
     sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
-    gamestarted = 0
     music.play(music.melodyPlayable(music.powerDown), music.PlaybackMode.UntilDone)
+    scene.setBackgroundImage(assets.image`nothing`)
+    gameover = sprites.create(assets.image`gameover`, SpriteKind.Decoration)
+    pause(100)
+    canyesno = 1
 })
 let fireball: Sprite = null
 let LevelNumber: Sprite = null
 let Level: Sprite = null
 let player1: Sprite = null
+let gameover: Sprite = null
 let gamestarted = 0
+let candostuff = 0
+let canyesno = 0
+canyesno = 0
+candostuff = 0
 sprites.destroyAllSpritesOfKind(SpriteKind.Player)
 scene.setBackgroundImage(assets.image`Starsbg`)
 let mySprite = sprites.create(assets.image`title`, SpriteKind.Decoration)
@@ -31,50 +43,67 @@ animation.animationPresets(animation.bobbing),
 2000,
 true
 )
-let candostuff = 1
+canyesno = 1
 game.onUpdate(function () {
-    if (controller.right.isPressed()) {
-        player1.x += 1
+    if (candostuff == 1) {
+        if (controller.right.isPressed()) {
+            player1.x += 1
+        }
     }
 })
 game.onUpdate(function () {
-    if (controller.right.isPressed()) {
-        player1.x += 1
+    if (candostuff == 1) {
+        if (controller.right.isPressed()) {
+            player1.x += 1
+        }
     }
 })
 game.onUpdate(function () {
-    if (controller.left.isPressed()) {
-        player1.x += -1
+    if (candostuff == 1) {
+        if (controller.left.isPressed()) {
+            player1.x += -1
+        }
     }
 })
 game.onUpdate(function () {
-    if (controller.left.isPressed()) {
-        player1.x += -1
+    if (candostuff == 1) {
+        if (controller.left.isPressed()) {
+            player1.x += -1
+        }
     }
 })
 game.onUpdate(function () {
-    if (controller.up.isPressed()) {
-        player1.y += -1
+    if (candostuff == 1) {
+        if (controller.up.isPressed()) {
+            player1.y += -1
+        }
     }
 })
 game.onUpdate(function () {
-    if (controller.up.isPressed()) {
-        player1.y += -1
+    if (candostuff == 1) {
+        if (controller.up.isPressed()) {
+            player1.y += -1
+        }
     }
 })
 game.onUpdate(function () {
-    if (controller.down.isPressed()) {
-        player1.y += 1
+    if (candostuff == 1) {
+        if (controller.down.isPressed()) {
+            player1.y += 1
+        }
     }
 })
 game.onUpdate(function () {
-    if (controller.down.isPressed()) {
-        player1.y += 1
+    if (candostuff == 1) {
+        if (controller.down.isPressed()) {
+            player1.y += 1
+        }
     }
 })
 forever(function () {
-    if (controller.A.isPressed() && (gamestarted == 0 && candostuff == 1)) {
+    if (controller.A.isPressed() && (gamestarted == 0 && canyesno == 1)) {
         gamestarted = 1
+        candostuff = 0
         sprites.destroyAllSpritesOfKind(SpriteKind.Decoration)
         music.stopAllSounds()
         scene.setBackgroundImage(assets.image`nothing`)
@@ -86,11 +115,16 @@ forever(function () {
         sprites.destroyAllSpritesOfKind(SpriteKind.Decoration)
         scene.setBackgroundImage(assets.image`Starsbg`)
         player1 = sprites.create(assets.image`player`, SpriteKind.Player)
+        candostuff = 1
         player1.setPosition(80, 81)
         player1.setStayInScreen(true)
         fireball = sprites.create(assets.image`fireball`, SpriteKind.Enemy)
         fireball.setBounceOnWall(true)
         fireball.setPosition(randint(0, 160), randint(0, 160))
+        if (player1.y - fireball.y < 20 || player1.x - fireball.x < 20) {
+            fireball.x += 20
+            fireball.y += 20
+        }
         fireball.setVelocity(player1.x - fireball.x, player1.y - fireball.y)
     }
 })
